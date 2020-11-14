8c7d87f5d5f751fa6261b42138bd29e2a21774fb
R_Hasan <sohandoabernahin1@gmail.com>
2020-11-14 23:36:21 +0600

    why so serious?

--- ART_OF_PROGRAMMING_CONTEST/CHAPTER_7__MATHEMATICS/343__What_Base_Is_This.cpp
+++ ART_OF_PROGRAMMING_CONTEST/CHAPTER_7__MATHEMATICS/343__What_Base_Is_This.cpp
@@ -63,8 +63,8 @@ void solve() {
     }    
 }
 int main() {
-    // ios_base::sync_with_stdio(0);
-    // cin.tie(0); cout.tie(0);
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
     solve();
     return 0;
 }
